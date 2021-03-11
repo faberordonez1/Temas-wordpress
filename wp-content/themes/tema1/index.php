baa03dd313caafdb6bf67fa957009d5c22fd356f
Faber Ordoñez <faberordonez1@gmail.com>
2021-03-10 22:37:57 -0500

    guia footer.php

--- wp-content/themes/tema1/index.php
+++ wp-content/themes/tema1/index.php
@@ -49,29 +49,5 @@
     </div>
     <!-- Fin Contenido -->
     
-    
-    <!-- Footer -->
-    <div class="container-fluid bg-dark text-white py-3">  <!-- 1 footer -->
-        <div class="container text-center">  <!-- 2 footer -->
-          <p><!-- 3 footer -->
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis officia delectus impedit tenetur magnam
-            corrupti, facere distinctio rep
-          </p>
-        </div>
-    </div>
-    <!-- Fin Footer -->
-
-     <!-- Bootstrap JS -->
-    
-    <!-- CDN -->
-    
-    <!--     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" 
-    crossorigin="anonymous"></script>
-     -->    
-
-    <!-- Archivos locales -->
-    <script src="js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+    <?php get_footer();?>
+  
