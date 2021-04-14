ae52a49f868158f9addc67b935481d3a349ec876
Faber Ordoñez <faberordonez1@gmail.com>
2021-04-13 22:10:47 -0500

    Loop Entradas

--- wp-content/themes/tema1/index.php
+++ wp-content/themes/tema1/index.php
@@ -4,33 +4,25 @@
     <div class="container my-5"> <!-- 1  Contenido-->
         <div class="row"> <!-- 2 Contenido-->
               <!-- Articulo -->
+              <?php 
+        if ( have_posts() ) : 
+            while ( have_posts() ) : the_post();?> 
               <div class="col-12 col-sm-6 col-md-4"><!-- 3 Contenido-->
                     <div class="card mb-3"> <!-- 4 y 7 Contenido-->
                         <img src="https://loremflickr.com/940/788" class="card-img-top img-fluid"><!-- 4 y 5 Contenido-->
                         <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="the_permalink();"><h5 class="card-title"><?php the_title();?></h5></a>
+                          <p class="card-text"><?php the_excerpt();?></p>
                         </div>
                         <div class="card-footer"><!-- 8  Contenido-->
-                          <small class="text-muted">Fecha / Categorias / Autor</small>
+                          <small class="text-muted"><?php echo get_the_date();?> / <?php the_category(',') ?> / <?php the_author()?></small>
                         </div>
                     </div>
-              </div>        
-              <!-- Fin Articulo -->    
-              <!-- Articulo --> <!-- 9 Contenido-->  
-              <div class="col-12 col-sm-6 col-md-4"><!-- 3 Contenido-->
-                    <div class="card mb-3"> <!-- 4 y 7 Contenido-->
-                        <img src="https://loremflickr.com/940/788" class="card-img-top img-fluid"><!-- 4 y 5 Contenido-->
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                        <div class="card-footer"><!-- 8  Contenido-->
-                          <small class="text-muted">Fecha / Categorias / Autor</small>
-                        </div>
-                    </div>
-              </div>        
-              <!-- Fin Articulo -->    
+              </div><?php 
+            endwhile; 
+          endif; 
+          ?>    
+              <!-- Fin Articulo -->     
               <!-- Articulo -->
               <div class="col-12 col-sm-6 col-md-4"><!-- 3 Contenido-->
                     <div class="card mb-3"> <!-- 4 y 7 Contenido-->
